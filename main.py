fef6cd53714b54fe00c490466cd24d79d5c051b3
Ashish Yadav <31487950+ashudva@users.noreply.github.com>
2022-02-05 22:59:09 +0530

    Update import

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from  sklearn.cross_validation import train_test_split
+from  sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
